5597a8ed7b1a245a762fd12c924f8220455474c4
asbtho <a_thom89@hotmail.com>
2018-08-01 16:33:49 +0200

    Add files via upload

---
+++ Get users who expires on a Date.ps1
@@ -0,0 +1,5 @@
+﻿# Set how many days ahead from todays date you want to check
+$ExpireMin = (Get-Date).AddDays(7);
+$ExpireMax = (Get-Date).AddDays(9);
+
+get-aduser -SearchBase "Ou DistinguishedName here" -filter 'enabled -eq $true' -properties AccountExpirationDate, LastLogonDate | Where-Object{$_.AccountExpirationDate -lt $ExpireMax -and $_.AccountExpirationDate -gt $ExpireMin} | Export-Csv c:\temp\usersWhoExpires.csv -encoding "unicode"
