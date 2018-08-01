bb3766ffb3357ac5093622e6a95bc3ff1a5555b9
asbtho <a_thom89@hotmail.com>
2018-08-01 16:26:14 +0200

    addComputers

---
+++ addComputers.ps1
@@ -0,0 +1,20 @@
+﻿#Get computers list from csv
+$complist = Import-Csv -Path "c:\temp1\PCList.csv" | ForEach-Object {$_.NetBIOSName}
+
+
+$users = Get-ADUser -filter * -SearchBase "OU distinguishedName here"
+
+#loop users in OU
+foreach($user in $users){
+    
+    #get existing computers from user
+    $comparray = (Get-AdUser $user -Properties LogonWorkstations).LogonWorkstations
+
+    #add computers from imported csv
+    foreach($comp in $complist){
+        $comparray += "," + $comp
+    }
+
+    #add computers to user
+    Set-ADUser $user -LogonWorkstations $comparray
+}
