e83fcbc5e2cd5dd38776d66f2bbd3d4269d5a4df
asbtho <a_thom89@hotmail.com>
2018-08-01 16:42:20 +0200

    Add files via upload

---
+++ Get-Uptime.ps1
@@ -0,0 +1,8 @@
+﻿function Get-Uptime {
+   $os = Get-WmiObject win32_operatingsystem
+   $uptime = (Get-Date) - ($os.ConvertToDateTime($os.lastbootuptime))
+   $Display = "Uptime: " + $Uptime.Days + " days, " + $Uptime.Hours + " hours, " + $Uptime.Minutes + " minutes" 
+   Write-Output $Display
+}
+
+Get-Uptime
