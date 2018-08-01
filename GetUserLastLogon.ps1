8c7a9eeaece9ceefc195d32df2665c88527525e8
asbtho <a_thom89@hotmail.com>
2018-08-01 16:44:13 +0200

    Add files via upload

---
+++ GetUserLastLogon.ps1
@@ -0,0 +1,4 @@
+﻿$hash_lastLogonTimestamp = @{Name="LastLogonTimeStamp";Expression={([datetime]::FromFileTime($_.LastLogonTimeStamp))}}
+$hash_pwdLastSet = @{Name="pwdLastSet";Expression={([datetime]::FromFileTime($_.pwdLastSet))}}
+ 
+get-aduser username -properties lastlogontimestamp,pwdLastSet | select samaccountname, $hash_lastLogonTimestamp,$hash_pwdLastSet
