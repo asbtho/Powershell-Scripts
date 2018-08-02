5274859ae7d6977de93f2302133dc8853c858154
asbtho <a_thom89@hotmail.com>
2018-08-02 09:21:27 +0200

    Add files via upload

---
+++ MachinesWithoutLogonOver30Days.ps1
@@ -0,0 +1,5 @@
+﻿asnp citrix*
+
+$logonDate = (get-date).AddDays(-30)
+ 
+Get-BrokerDesktop -maxrecordcount 1000 | Select-Object MachineName,{$_.AssociatedUserNames},LastConnectionTime | where-object {$_.LastConnectionTime -le $logonDate} | Export-Csv -Path "MachinesWithoutLogonOver30Days.csv"
