8b0c8623e31ab14688289a8ea015747b648d4819
asbtho <a_thom89@hotmail.com>
2019-03-22 14:55:39 +0100

    Add files via upload

---
+++ findBrokenSIDCitrix.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Importing the Citrix Modules..."
+asnp Citrix*
+ 
+$PublishedApps = Get-BrokerApplication | Where {$_.AssociatedUserNames -like "S-1-5-21*"}
+Foreach($app in $PublishedApps)
+{
+Write-Host ” –” $app.Name "is broken"
+}
