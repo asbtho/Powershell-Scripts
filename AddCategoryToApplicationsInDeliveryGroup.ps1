ebe74afb874ddd4f37d521666ca0afa79d5cce5c
asbtho <a_thom89@hotmail.com>
2019-05-03 14:06:04 +0200

    Add files via upload

---
+++ AddCategoryToApplicationsInDeliveryGroup.ps1
@@ -0,0 +1,7 @@
+﻿asnp Citrix*
+$desktopgroup = Get-BrokerDesktopGroup -Name "TEST - Teknisk 2016"
+$DesktopGroupUid = $DesktopGroup.Uid
+
+foreach ($Application in (Get-BrokerApplication -AssociatedDesktopGroupUid $DesktopGroupUid -MaxRecordCount 9999)) {
+    Set-BrokerApplication -InputObject $Application -ClientFolder "Windows Server 2016 TEST"
+}
