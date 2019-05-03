16d884a744de870a3bc77ed9fedfb3e104bff9c4
asbtho <a_thom89@hotmail.com>
2019-05-03 10:35:18 +0200

    Add files via upload

---
+++ AddGroupToApplicationsInDeliveryGroup.ps1
@@ -0,0 +1,7 @@
+﻿asnp Citrix*
+$desktopgroup = Get-BrokerDesktopGroup -Name "Teknisk"
+$DesktopGroupUid = $DesktopGroup.Uid
+
+foreach ($Application in (Get-BrokerApplication -AssociatedDesktopGroupUid $DesktopGroupUid -MaxRecordCount 9999)) {
+    Add-BrokerUser "teknisk\TG-Citrix TEST 2016" -Application $Application.Name
+}
---
+++ GetApplicationsFromDesktopGroup-Report.ps1
@@ -0,0 +1,4 @@
+﻿asnp Citrix*
+$desktopgroup = Get-BrokerDesktopGroup -Name "Teknisk"
+$DesktopGroupUid = $DesktopGroup.Uid
+Get-BrokerApplication -AssociatedDesktopGroupUid $DesktopGroupUid | Export-CSV c:\temp\application101.csv
