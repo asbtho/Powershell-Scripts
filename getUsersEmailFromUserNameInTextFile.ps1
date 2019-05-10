a6e9fad2f77d79ec4eeafc668d5fef80b9907d73
asbtho <a_thom89@hotmail.com>
2019-05-10 14:25:55 +0200

    Add files via upload

---
+++ getUsersEmailFromUserNameInTextFile.ps1
@@ -0,0 +1,5 @@
+﻿$users = ForEach ($user in $(Get-Content C:\temp\users.txt)) {
+    Get-AdUser $user -Properties Department,Mail  
+}
+ 
+$users | Select-Object SamAccountName,Department,Mail | Export-CSV -Path C:\temp\output.csv -NoTypeInformation
