b4695267065b24056d920ca06f583678b73da60a
asbtho <a_thom89@hotmail.com>
2020-02-10 16:35:45 +0100

    Update GetApplicationsFromDesktopGroup-Report.ps1

--- GetApplicationsFromDesktopGroup-Report.ps1
+++ GetApplicationsFromDesktopGroup-Report.ps1
@@ -1,4 +1,6 @@
 ﻿asnp Citrix*
 $desktopgroup = Get-BrokerDesktopGroup -Name "Teknisk"
 $DesktopGroupUid = $DesktopGroup.Uid
-Get-BrokerApplication -AssociatedDesktopGroupUid $DesktopGroupUid | Export-CSV c:\temp\application101.csv
+Get-BrokerApplication -AssociatedDesktopGroupUid $DesktopGroupUid | Export-CSV c:\temp\application101.csv -Encoding UTF8
+# For object properties use Select-Object. Example:
+# Get-BrokerApplication -AssociatedDesktopGroupUid $DesktopGroupUid | Select-Object PublishedName,{$_.AssociatedUserNames} | Export-CSV c:\temp\application101.csv -Encoding UTF8
