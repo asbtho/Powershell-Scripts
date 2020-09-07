1ad53bdfa998e584e6dd8fa8aa454c91f769daa1
asbtho <a_thom89@hotmail.com>
2020-09-07 15:23:40 +0200

    Add files via upload

---
+++ removeUserProfileBackups.ps1
@@ -0,0 +1,11 @@
+﻿$pattern = ".old"
+$profiles = gci -path 'c:\TestFolder\'
+foreach ($userprof in $profiles){
+    #Write-Host $userprof.FullName 
+    if ($userprof.FullName -match $pattern){
+            # Uncomment for read only logging
+        Write-Host $userprof.FullName 
+            # Uncomment for deletion
+        remove-item $userprof.FullName -Recurse -Force
+    }
+}
---
+++ removeUserProfilesLastWrite90Days.ps1
@@ -0,0 +1,10 @@
+﻿$profiles = gci -path '\\server\share\'
+foreach ($userprof in $profiles){
+    $lastwrite = get-item ($userprof.FullName + '\UPM_Profile\ntuser.dat') -Force
+    if ((((get-date) - ($lastwrite).LastWriteTime).days) -ge 90){
+            # Uncomment for read only logging
+        #Write-Host $userprof.FullName 
+            # Uncomment for deletion
+        #remove-item $userprof.FullName -Recurse -Force
+    }
+}
