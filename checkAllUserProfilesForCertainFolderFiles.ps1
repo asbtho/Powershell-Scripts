f9613c583ffdc8a31dd6bd92c2f5a87bd3be25c5
asbtho <a_thom89@hotmail.com>
2022-01-19 16:50:24 +0100

    Add files via upload

---
+++ checkAllUserProfilesForCertainFolderFiles.ps1
@@ -0,0 +1,15 @@
+﻿#Script to find if certain folder in user profile share contains files or is empty.
+
+$path = "H:\Users\"
+
+$parentDirs = Get-ChildItem -Path $path -Directory -Force -ErrorAction SilentlyContinue
+
+foreach ($dir in $parentDirs){
+    $filename = '*.*'
+    $searchinfolder = $path + $dir + "\Chrome\UserData\Default\blob_storage*"
+    if (Test-Path -Path $searchinfolder) {
+        Get-ChildItem -Path $searchinfolder -Filter $filename -Recurse | %{$_.FullName}
+    } else {
+        #Path doesn't exist.
+    }
+}
